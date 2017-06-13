bf913d1fe88a1288897356419a27ba17765ea386
Xenon Chau <cto@ido-game.com>
2017-06-13 11:17:08 +0800

    react libs for android lib

---
+++ com.facebook.react/src/com/example/com/facebook/react/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.com.facebook.react;
+
+
+
+//public class MainActivity extends Activity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+// 
+//}
